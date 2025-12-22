394efeac15b3b319b8e5c4993a71f3603a1723f1
iveflava <torbokov.artem@anextour.com>
2025-12-22 21:10:55 +0300

    revert

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,55 +1,54 @@
 import {
-  memo, useEffect, useRef
+  memo, useEffect, useRef, useState
 } from 'react'
-// import { AppRouter } from '@/providers/AppRouter'
-// import { Toaster } from '@/features/Toaster/ui/Toaster'
+import { AppRouter } from '@/providers/AppRouter'
+import { Toaster } from '@/features/Toaster/ui/Toaster'
 import '../styles/fonts.scss'
 import '../styles/index.scss'
 import { useAppDispatch, useAppSelector } from '@/shared/utils/hooks'
 import { selectPending } from '@/model/global/GlobalSelectors'
-// import { setPending } from '@/model/global/GlobalSlice'
+import { setPending } from '@/model/global/GlobalSlice'
 import { Loader } from '@/features/Loader/Loader'
-// import sLoader from '@/features/Loader/Loader.module.scss'
+import sLoader from '@/features/Loader/Loader.module.scss'
 
 export const App = memo(() => {
   const ref = useRef<HTMLDivElement>(null)
   const dispatch = useAppDispatch()
 
   const pending = useAppSelector(selectPending)
-  // const [renderApp, setRenderApp] = useState(false)
+  const [renderApp, setRenderApp] = useState(false)
 
   useEffect(() => {
-    throw new Error('Test error')
-    // const onAnimationEnd = (e: AnimationEvent) => {
-    //   const { animationName } = e
-    //   if (animationName?.toLowerCase()?.includes('disappearance') && ref.current) {
-    //     ref.current.removeEventListener('animationend', onAnimationEnd)
-    //     dispatch(setPending(false))
-    //   }
-    // }
+    const onAnimationEnd = (e: AnimationEvent) => {
+      const { animationName } = e
+      if (animationName?.toLowerCase()?.includes('disappearance') && ref.current) {
+        ref.current.removeEventListener('animationend', onAnimationEnd)
+        dispatch(setPending(false))
+      }
+    }
 
-    // document.fonts.ready.then(() => {
-    //   setRenderApp(true)
-    //   setTimeout(() => {
-    //     if (!ref?.current) { return }
-    //     ref.current.addEventListener('animationend', onAnimationEnd)
-    //     ref.current.classList.add(sLoader.img_animate_disappearance)
-    //   }, 0)
-    // }, () => {
-    //   dispatch(setPending(false))
-    //   setRenderApp(true)
-    // })
+    document.fonts.ready.then(() => {
+      setRenderApp(true)
+      setTimeout(() => {
+        if (!ref?.current) { return }
+        ref.current.addEventListener('animationend', onAnimationEnd)
+        ref.current.classList.add(sLoader.img_animate_disappearance)
+      }, 0)
+    }, () => {
+      dispatch(setPending(false))
+      setRenderApp(true)
+    })
   }, [dispatch])
 
   return (
     <>
       {pending && <Loader ref={ref} />}
-      {/* {renderApp && (
+      {renderApp && (
         <>
           <AppRouter />
           <Toaster />
         </>
-      )} */}
+      )}
     </>
   )
 })
