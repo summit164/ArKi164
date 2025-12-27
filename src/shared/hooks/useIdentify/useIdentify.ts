d2c903ec8ea8b7e7555d077896a806ac8216380b
iveflava <torbokov.artem@anextour.com>
2025-12-27 19:46:14 +0300

    useIdentify added

--- src/shared/hooks/useIdentify/useIdentify.ts
+++ src/shared/hooks/useIdentify/useIdentify.ts
@@ -3,12 +3,14 @@ import { useEffect } from 'react'
 
 export const useIdentify = () => {
   useEffect(() => {
-    fetch('https://xzbkxthnfksriubmhlfx.supabase.co/functions/v1/identify', {
-      headers: {
-        method: 'POST',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ tgName: WebApp.initDataUnsafe.user?.username })
-    })
+    if (process.env.REACT_APP_MODE === 'production') {
+      fetch('https://xzbkxthnfksriubmhlfx.supabase.co/functions/v1/identify', {
+        headers: {
+          method: 'POST',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ tgName: WebApp.initDataUnsafe.user?.username })
+      })
+    }
   }, [])
 }
