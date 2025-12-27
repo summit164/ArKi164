0867c715085c52cac77db22a86f21744e944fca4
iveflava <torbokov.artem@anextour.com>
2025-12-27 19:55:37 +0300

    test

--- src/shared/hooks/useIdentify/useIdentify.ts
+++ src/shared/hooks/useIdentify/useIdentify.ts
@@ -2,17 +2,21 @@ import WebApp from '@twa-dev/sdk'
 import { useEffect } from 'react'
 
 export const useIdentify = () => {
-  useEffect(() => {
+  const identify = async () => {
     console.log(1)
     if (process.env.REACT_APP_MODE === 'production') {
       console.log(123)
-      fetch('https://xzbkxthnfksriubmhlfx.supabase.co/functions/v1/identify', {
+      const r = await fetch('https://xzbkxthnfksriubmhlfx.supabase.co/functions/v1/identify', {
         headers: {
           method: 'POST',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({ tgName: WebApp.initDataUnsafe.user?.username })
       })
+      console.log(r)
     }
+  }
+  useEffect(() => {
+    identify()
   }, [])
 }
