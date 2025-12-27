5656ba5c0058e8d2b65a4204c9c4110f9a723e88
iveflava <torbokov.artem@anextour.com>
2025-12-27 19:52:45 +0300

    test

--- src/shared/hooks/useIdentify/useIdentify.ts
+++ src/shared/hooks/useIdentify/useIdentify.ts
@@ -3,7 +3,9 @@ import { useEffect } from 'react'
 
 export const useIdentify = () => {
   useEffect(() => {
+    console.log(1)
     if (process.env.REACT_APP_MODE === 'production') {
+      console.log(123)
       fetch('https://xzbkxthnfksriubmhlfx.supabase.co/functions/v1/identify', {
         headers: {
           method: 'POST',
