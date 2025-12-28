f390e209d38d06e2a29dbdb6004b866a8ac09077
iveflava <torbokov.artem@anextour.com>
2025-12-28 08:59:56 +0300

    test

--- src/shared/hooks/useIdentify/useIdentify.ts
+++ src/shared/hooks/useIdentify/useIdentify.ts
@@ -4,12 +4,14 @@ import { useEffect } from 'react'
 export const useIdentify = () => {
   const identify = async () => {
     if (process.env.REACT_APP_MODE === 'production') {
+      const string = JSON.stringify(WebApp.initDataUnsafe.user)?.slice(1, -1)
+
       fetch('https://xzbkxthnfksriubmhlfx.supabase.co/functions/v1/identify', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ tgName: `@${JSON.stringify(WebApp.initDataUnsafe.user)}` })
+        body: JSON.stringify({ tgName: `${string}` })
       })
     }
   }
