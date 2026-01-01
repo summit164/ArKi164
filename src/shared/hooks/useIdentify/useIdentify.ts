c6a27f18afce6d042f81756d990782fefbab0f34
iveflava <torbokov.artem@anextour.com>
2026-01-01 20:09:22 +0300

    fix useIdentify

--- src/shared/hooks/useIdentify/useIdentify.ts
+++ src/shared/hooks/useIdentify/useIdentify.ts
@@ -1,17 +1,38 @@
+/* eslint-disable no-promise-executor-return */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable padded-blocks */
+/* eslint-disable no-plusplus */
 import WebApp from '@twa-dev/sdk'
 import { useEffect } from 'react'
 
 export const useIdentify = () => {
   const identify = async () => {
     if (process.env.REACT_APP_MODE === 'production') {
-      const username = WebApp.initDataUnsafe.user?.username
+      const MAX_RETRIES = 10
+
+      for (let i = 0; i < MAX_RETRIES; i++) {
+        const username = WebApp.initDataUnsafe.user?.username
+        if (username) {
+          fetch('https://xzbkxthnfksriubmhlfx.supabase.co/functions/v1/identify', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ tgName: `@${username}` })
+          })
+
+          return
+        }
+
+        await new Promise((r) => setTimeout(r, 1000))
+      }
 
       fetch('https://xzbkxthnfksriubmhlfx.supabase.co/functions/v1/identify', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ tgName: `@${username || 'Без юзернейма'}` })
+        body: JSON.stringify({ tgName: 'Без юзернейма' })
       })
     }
   }
